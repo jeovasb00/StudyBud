36b2b07765abe7c806f2a176152334f46a7762df
Jeová de Sousa Barbosa <jeovasousabarbosassj@gmail.com>
2025-02-17 17:30:53 -0300

    criando model para salas (Rooms)

--- base/models.py
+++ base/models.py
@@ -6,4 +6,10 @@ class Room(models.Model):
     topic =
     '''
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
+    # paticipants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.name
